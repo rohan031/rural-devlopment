984df4904afd0cff712eeb1a91f4f1f1f1c4a901
rohan <rohanverma031@gmail.com>
2024-07-21 16:13:20 +0530

    updated list

--- src/app/non-realisations/coordination-et-gestion-du-projet/page.tsx
+++ src/app/non-realisations/coordination-et-gestion-du-projet/page.tsx
@@ -4,21 +4,23 @@ import Container from "@/components/Container/Container";
 import styles from "./page.module.scss";
 
 const data = {
-    heading: "COORDINATION ET GESTION DU PROJET",
-    text: "Élaboration des rapports d’avancement, d’audit et de finances ;",
+	heading: "COORDINATION ET GESTION DU PROJET",
+	text: ["Élaboration des rapports d’avancement, d’audit et de finances ;"],
 };
 
 const ProjectCoordinationAndManagement = () => {
-    return (
-        <>
-            <Hero heading={data.heading} />
-            <Container className={styles.list}>
-                <ul>
-                    <li>{data.text}</li>
-                </ul>
-            </Container>
-        </>
-    );
+	return (
+		<>
+			<Hero heading={data.heading} />
+			<Container className={styles.list}>
+				<ul>
+					{data.text.map((item) => (
+						<li key={item}>{item}</li>
+					))}
+				</ul>
+			</Container>
+		</>
+	);
 };
 
 export default ProjectCoordinationAndManagement;
