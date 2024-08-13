34fb8b5e2b0c9701e6963e5d050e9796315d5e3f
rohan <rohanverma031@gmail.com>
2024-08-13 11:54:44 +0530

    fix: contact us page implementation fixed

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,72 +1,109 @@
 import React from "react";
-import style from "./page.module.scss";
+import styles from "./page.module.scss";
 import Container from "@/components/Container/Container";
 import Image from "next/image";
-import Link from "next/link";
+import { address, email, phone } from "@/data/navigation";
 
 const data = {
-    heading: "CONTACTEZ-NOUS",
-    add: "Avenue Lukusa, n°111-112, croisement des avenues TSF et Lukusa,dans l’enceinte du secrétariat Général au Développement Rural",
-    phone1: "(+243) 817 073 112",
-    phone2: "(+243) 817 073 118",
-    email: "projetpriserdc@prise-rdc.com",
+	heading: "CONTACTEZ-NOUS",
 };
 
 const Contact = () => {
-    return (
-        <div className={style.contact}>
-            <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3978.561027560374!2d15.299146400000001!3d-4.30507!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1a6a33ad477ec485%3A0xeba82966c64fe33!2sProjet%20de%20Renforcement%20des%20Infrastructures%20Socio-Economiques%20dans%20la%20zone%20Centre%20de%20la%20RDC!5e0!3m2!1sen!2sin!4v1721113994812!5m2!1sen!2sin"
-                style={{ border: 0 }}
-                loading="lazy"
-                width="0"
-                height="0"
-            ></iframe>
-            <Container className={style.content}>
-                <h1>{data.heading}</h1>
-                <div className={style.details}>
-                    <div className={style.img}>
-                        <Image
-                            src="/home/blogs/blog6.webp"
-                            width={415}
-                            height={440}
-                            alt="contact"
-                        />
-                    </div>
-                    <div className={style.add}>
-                        <div>
-                            <h2>Adresse physique</h2>
-                            <p>{data.add}</p>
-                        </div>
-                        <div>
-                            <h2>Téléphone</h2>
-                            <div>
-                                <Link href={`tel:${data.phone1}`}>
-                                    {data.phone1}
-                                </Link>
-                                <Link href={`tel:${data.phone2}`}>
-                                    {data.phone2}
-                                </Link>
-                            </div>
-                        </div>
-                        <div>
-                            <h2>Adresse e-mail</h2>
-                            <Link href={`mailto:${data.email}`}>
-                                {data.email}
-                            </Link>
-                        </div>
-                    </div>
-                    <form>
-                        <input type="text" placeholder="Name" />
-                        <input type="text" placeholder="Phone" />
-                        <input type="text" placeholder="Email" />
-                        <textarea placeholder="Message" />
-                        <button>Submit</button>
-                    </form>
-                </div>
-            </Container>
-        </div>
-    );
+	return (
+		<>
+			<div className={styles.iframe}>
+				<iframe
+					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3978.561027560374!2d15.299146400000001!3d-4.30507!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1a6a33ad477ec485%3A0xeba82966c64fe33!2sProjet%20de%20Renforcement%20des%20Infrastructures%20Socio-Economiques%20dans%20la%20zone%20Centre%20de%20la%20RDC!5e0!3m2!1sen!2sin!4v1721113994812!5m2!1sen!2sin"
+					style={{ border: 0 }}
+					loading="lazy"
+					width="0"
+					height="0"
+				></iframe>
+			</div>
+
+			<Container className={styles.content}>
+				<h1>{data.heading}</h1>
+
+				<div className={styles.details}>
+					<div className={styles.image}>
+						<Image
+							src="/home/blogs/blog6.webp"
+							width={415}
+							height={400}
+							alt="contact"
+						/>
+					</div>
+
+					<div className={styles.contact}>
+						<div className={styles.address}>
+							<h3>Adresse physique</h3>
+
+							<p>{address}</p>
+						</div>
+
+						<div className={styles.phone}>
+							<h3>Téléphone</h3>
+
+							<div className={styles.info}>
+								{phone.map((item) => {
+									return (
+										<div key={item}>
+											<a
+												data-type="link"
+												href={`tel:${item}`}
+											>
+												{item}
+											</a>
+										</div>
+									);
+								})}
+							</div>
+						</div>
+
+						<div className={styles.email}>
+							<h3>Adresse e-mail</h3>
+
+							<div className={styles.info}>
+								{email.map((item) => {
+									return (
+										<div key={item}>
+											<a
+												data-type="link"
+												href={`mailto:${item}`}
+											>
+												{item}
+											</a>
+										</div>
+									);
+								})}
+							</div>
+						</div>
+					</div>
+
+					<form className={styles.form}>
+						<div className={styles.input}>
+							<input type="text" placeholder="Name" />
+						</div>
+						<div className={styles.input}>
+							<input type="text" placeholder="Phone" />
+						</div>
+						<div className={styles.input}>
+							<input type="text" placeholder="Email" />
+						</div>
+						<div className={styles.input}>
+							<textarea placeholder="Message" />
+						</div>
+
+						<div className={styles.action}>
+							<button data-type="button" data-variant="primary">
+								Submit
+							</button>
+						</div>
+					</form>
+				</div>
+			</Container>
+		</>
+	);
 };
 
 export default Contact;
