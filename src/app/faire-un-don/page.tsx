256233aafbd109c84662613d9eae19902eacbb71
rohan <rohanverma031@gmail.com>
2024-11-24 19:35:05 +0530

    feat: donation page under construction

---
+++ src/app/faire-un-don/page.tsx
@@ -0,0 +1,17 @@
+import Container from "@/components/Container/Container";
+import React from "react";
+
+const DonationPage = () => {
+	return (
+		<Container
+			style={{
+				paddingBlockStart: "10rem",
+				textAlign: "center",
+			}}
+		>
+			<h2>Page Under Construction</h2>
+		</Container>
+	);
+};
+
+export default DonationPage;
